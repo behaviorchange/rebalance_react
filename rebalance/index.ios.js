bd0fe226ca1dc4291f0534577fc2596cbc68e227
Ben Mathes <mathes.ben@gmail.com>
2015-06-09 13:02:45 -0700

    initial react native commit

---
+++ rebalance/index.ios.js
@@ -0,0 +1,64 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
+'use strict';
+
+var React = require('react-native');
+var {
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View,
+} = React;
+
+var rebalance = React.createClass({
+    render: function() {
+        return (
+                <ListView style={styles.container}>
+                <Text style={styles.instructions}>
+                Hi there!
+            </Text>
+                </View>
+        );
+    }
+
+    renderWellnessSource: function(wellnessSource) {
+
+    }
+
+    
+});
+
+
+var WELLNESS_SOURCES = [
+    'exercise',
+    'diet/nutrition',
+    'time in nature',
+    'relationships',
+    'recreation/play',
+    'relaxation + stress management',
+    'religious/spiritual involvement',
+    'contribution + service'
+];
+
+var styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
+});
+
+AppRegistry.registerComponent('rebalance', () => rebalance);
